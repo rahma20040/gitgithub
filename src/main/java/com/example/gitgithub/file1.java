d8c4d13dfa69cbbeeae526d07b5583325efbbe42
rahma <rahmakhessibieniso@gmail.com>
2025-12-01 17:35:06 +0100

    aaaaa

--- src/main/java/com/example/gitgithub/file1.java
+++ src/main/java/com/example/gitgithub/file1.java
@@ -12,9 +12,6 @@ public class file1 {
         this.message = message;
     }
     
-    public String getAuthor() {
-        return author;
-    }
     
     public void setAuthor(String author) {
         this.author = author;
