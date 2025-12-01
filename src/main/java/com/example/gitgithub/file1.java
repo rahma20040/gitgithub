cc83e8ba065aa7fe9add7ed5b4da68a1c28e73ca
rahma <rahmakhessibieniso@gmail.com>
2025-12-01 18:48:05 +0100

    raaaaaaaaaaaaaa

--- src/main/java/com/example/gitgithub/file1.java
+++ src/main/java/com/example/gitgithub/file1.java
@@ -11,7 +11,15 @@ public class file1 {
     public void setMessage(String message) {
         this.message = message;
     }
+package com.example.gitgithub;
+
+public class file1 {
+    private String message = "Hello, GitHub! - Version Main";
+    private String author = "Mohamed Yassine Derbel";
+    private String version = "1.0.0";  // NOUVEAU
     
+    // ... rest du code
+}    
     
     public void setAuthor(String author) {
         this.author = author;
